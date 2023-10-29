abb70ea1ba6848004e50e83472302b0472bdd1a4
AdilHameed351 <59979658+AdilHameed351@users.noreply.github.com>
2023-10-29 22:34:33 +0500

    Update conn.js

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose");
 
 const conn = async (req, res) => {
     try {
-        await mongoose.connect("mongodb+srv://user:password123123@cluster0.vph15tu.mongodb.net/todolist?retryWrites=true&w=majority", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }).then(() => {
+        await mongoose.connect("mongodb+srv://user:password123123@cluster0.vph15tu.mongodb.net/")
+        .then(() => {
             console.log("Connected");
         });
     } catch (error) {
